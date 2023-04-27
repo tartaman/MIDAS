6d4f75e29d8cf692c867ba1d6a9cfba723f11051
Ventthos <ferra04@live.com.mx>
2023-04-27 15:10:27 -0600

    Gizmo de escala

--- Proyectos2/Assets/Scripts/ScaleGizmo.cs
+++ Proyectos2/Assets/Scripts/ScaleGizmo.cs
@@ -7,6 +7,10 @@ public class ScaleGizmo : MonoBehaviour
     Gizmo helper;
     [SerializeField] private char eje; 
     [SerializeField] GameObject seleccionado;
+    bool check;
+    Vector2 inicial;
+    Vector2 final;
+    Vector2 direccion;
     void Start()
     {
         helper = GameObject.FindGameObjectWithTag("helper").GetComponent<Gizmo>();
@@ -26,8 +30,82 @@ public class ScaleGizmo : MonoBehaviour
         switch (eje)
         {
             case 'X':
-                seleccionado.transform.localScale += Vector3.right * 5 * mouseX * Time.deltaTime;
+                if (Camera.main.transform.rotation.eulerAngles.y < 315 && Camera.main.transform.rotation.eulerAngles.y >= 225)
+                {
+                    seleccionado.transform.localScale += Vector3.right * -mouseY * Time.deltaTime * 5;
+                }
+                else if (Camera.main.transform.rotation.eulerAngles.y < 225 && Camera.main.transform.rotation.eulerAngles.y >= 135)
+                {
+                    seleccionado.transform.localScale += Vector3.right * -mouseX * Time.deltaTime * 5;
+                }
+                else if (Camera.main.transform.rotation.eulerAngles.y < 135 && Camera.main.transform.rotation.eulerAngles.y >= 45)
+                {
+                    seleccionado.transform.localScale += Vector3.right * mouseY * Time.deltaTime * 5;
+                }
+                else
+                {
+                    seleccionado.transform.localScale += Vector3.right * mouseX * Time.deltaTime * 5;
+                }
                 break;
+
+            case 'Y':
+                seleccionado.transform.localScale += Vector3.up * mouseY * Time.deltaTime * 5;
+                break;
+
+            case 'Z':
+                if (Camera.main.transform.rotation.eulerAngles.y < 315 && Camera.main.transform.rotation.eulerAngles.y >= 225)
+                {
+                    seleccionado.transform.localScale += Vector3.forward * mouseX * Time.deltaTime * 5;
+                }
+                else if (Camera.main.transform.rotation.eulerAngles.y < 225 && Camera.main.transform.rotation.eulerAngles.y >= 135)
+                {
+                    seleccionado.transform.localScale += Vector3.forward * -mouseY * Time.deltaTime * 5;
+                }
+                else if (Camera.main.transform.rotation.eulerAngles.y < 135 && Camera.main.transform.rotation.eulerAngles.y >= 45)
+                {
+                    seleccionado.transform.localScale += Vector3.forward * -mouseX * Time.deltaTime * 5;
+                }
+                else
+                {
+                    seleccionado.transform.localScale += Vector3.forward * mouseY * Time.deltaTime * 5;
+                }
+                break;
+            case 'G':
+                
+                if (check)
+                {
+                    final = (Vector2)Input.mousePosition;
+                    
+                    final = Camera.main.ScreenToViewportPoint(final);
+                    Debug.Log(final);
+                    //direccion = final - inicial;
+
+                    Debug.Log(inicial);
+                    /*
+                    if ((final.x > inicial.x || final.y > inicial.y) && !(final.y > inicial.y && final.x < inicial.x))
+                        seleccionado.transform.localScale += Vector3.one * Time.deltaTime;
+                    else if ((final.x < inicial.x || final.y < inicial.y) && !(final.y < inicial.y && final.x > inicial.x))
+                        seleccionado.transform.localScale -= Vector3.one * Time.deltaTime;
+                    */
+                    (float, char) aComparar = (final.x > final.y) ? (final.x, 'x') : (final.y, 'y');
+                    if (aComparar.Item2 == 'x' && aComparar.Item1 > inicial.x || aComparar.Item2 == 'y' && aComparar.Item1 > inicial.y)
+                        seleccionado.transform.localScale += Vector3.one * Time.deltaTime;
+                    else if (aComparar.Item2 == 'x' && aComparar.Item1 < inicial.x || aComparar.Item2 == 'y' && aComparar.Item1 < inicial.y)
+                        seleccionado.transform.localScale -= Vector3.one * Time.deltaTime;
+                    break;
+                    
+                   
+                }
+                else
+                {
+                    inicial = (Vector2)Input.mousePosition;
+                    inicial = Camera.main.ScreenToViewportPoint(inicial);
+                    check = true;
+
+                }
+                break;
+                
+                
             default:
                 break;
         }
